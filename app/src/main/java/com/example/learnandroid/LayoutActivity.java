ea5d9fa7604cb81a0574687b0c5c681faba9b748
robocon321 <robocon321n@gmail.com>
2021-01-27 00:32:33 +0700

    Learn linearlayout

---
+++ app/src/main/java/com/example/learnandroid/LayoutActivity.java
@@ -0,0 +1,35 @@
+package com.example.learnandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class LayoutActivity extends AppCompatActivity {
+    Button btnLinearLayout, btnRelativeLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout);
+        addControls();
+        setEvents();
+    }
+
+    public void addControls(){
+        btnLinearLayout = findViewById(R.id.btnLinearLayout);
+        btnRelativeLayout = findViewById(R.id.btnRelativeLayout);
+    }
+
+    public void setEvents(){
+        btnLinearLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LayoutActivity.this, LinearLayoutActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/learnandroid/MainActivity.java
+++ app/src/main/java/com/example/learnandroid/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.learnandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addControls();
+        setEvents();
+    }
+
+    public void addControls(){
+        btnLayout = findViewById(R.id.btnLayout);
+    }
+
+    public void setEvents(){
+    btnLayout.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent =  new Intent(MainActivity.this, LayoutActivity.class);
+            startActivity(intent);
+        }
+    });
     }
 }
