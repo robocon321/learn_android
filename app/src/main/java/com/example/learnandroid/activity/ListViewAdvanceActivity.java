112d0a77cee942a59f0c387ee95c7e51c5c875d3
robocon321 <robocon321n@gmail.com>
2021-01-27 23:34:34 +0700

    Complete example ListView advance

--- app/src/main/java/com/example/learnandroid/activity/ListViewAdvanceActivity.java
+++ app/src/main/java/com/example/learnandroid/activity/ListViewAdvanceActivity.java
@@ -6,14 +6,31 @@ import android.os.Bundle;
 import android.widget.ListView;
 
 import com.example.learnandroid.R;
+import com.example.learnandroid.adapter.CourseAdapter;
+import com.example.learnandroid.utils.Course;
+
+import java.util.ArrayList;
 
 
 public class ListViewAdvanceActivity extends AppCompatActivity {
     ListView lvItemAdvance;
+    ArrayList<Course> list;
+    CourseAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view_advance);
+
+        list = new ArrayList<>();
+
+        list.add(new Course("Android", 499, R.drawable.android));
+        list.add(new Course("Ubuntu", 299, R.drawable.ubuntu));
+        list.add(new Course("Windows", 199, R.drawable.windows));
+        list.add(new Course("MacOS", 249, R.drawable.computer));
+
+        adapter = new CourseAdapter(this, R.layout.course_layout, list);
+        lvItemAdvance = findViewById(R.id.lvItemAdvance);
+        lvItemAdvance.setAdapter(adapter);
     }
 }
